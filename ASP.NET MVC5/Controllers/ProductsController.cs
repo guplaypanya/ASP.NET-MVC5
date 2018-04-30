26775d7bd014bad866ee015d71c37a64887bd47a
Thanapon Laopon <Thanaponl57@email.nu.ac.th>
2018-04-30 12:01:43 +0700

    Add - a new product to MSSQL

---
+++ ASP.NET MVC5/Controllers/ProductsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASP.NET_MVC5.Models;
+
+namespace ASP.NET_MVC5.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public NorthwindDbcontext _context;
+
+        public ProductsController()
+        {
+            _context = new NorthwindDbcontext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Save(Product product)
+        {
+            _context.Products.Add(product);
+            _context.SaveChanges();
+
+            return Redirect("Index");
+        }
+
+    }
+}
