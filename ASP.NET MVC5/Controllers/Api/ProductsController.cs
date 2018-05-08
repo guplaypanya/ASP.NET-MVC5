1d5bd3f5bbd6ef23e3c8b32712a1ae259cc8bb93
Thanapon Laopon <Thanaponl57@email.nu.ac.th>
2018-05-08 14:57:31 +0700

    Serialization json and query data through api controller

--- ASP.NET MVC5/Controllers/Api/ProductsController.cs
+++ ASP.NET MVC5/Controllers/Api/ProductsController.cs
@@ -1,13 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using ASP.NET_MVC5.Models;
 
 namespace ASP.NET_MVC5.Controllers.Api
 {
     public class ProductsController : ApiController
     {
+        private NorthwindDbcontext _context;
+
+        public ProductsController()
+        {
+            _context = new NorthwindDbcontext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        public IHttpActionResult GetProducts()
+        {
+            var products = _context.Products
+                .Select(
+                    p =>
+                        new
+                        {
+                            p.ProductID,
+                            p.ProductName,
+                            p.Category.CategoryName
+                        }
+                );
+
+            if (products == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(products);
+        }
+
     }
 }
